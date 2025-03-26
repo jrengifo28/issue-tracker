f741b27c9255dbb496b5df12ae6f030b5fc3708e
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2025-03-25 23:57:39 -0500

    Recfactor de loading page, add a spinner

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,12 @@
+import { Spinner } from "@radix-ui/themes";
 import React from "react";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 };
 
 export default LoadingIssueDetailPage;
