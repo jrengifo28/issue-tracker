e62d4c94382ff91d954e899950288375d0517177
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2025-03-31 00:49:09 -0500

    Improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { prisma } from "@/prisma/client";
-import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
 import IssueFormSkeleton from "./loading";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
-  ssr: false,
+  ssr: true,
   loading: () => <IssueFormSkeleton />,
 });
 
